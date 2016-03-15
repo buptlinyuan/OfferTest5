31fce8df81d3cc2f57e2fe50951df9cd161efe63
toryang <ToryangChen@outlook.com>
2016-03-15 21:26:45 +0800

    Initial commit

---
+++ src/TreeLinkNode.java
@@ -0,0 +1,13 @@
+/**
+ * Created by toryang on 16/3/15.
+ */
+public class TreeLinkNode {
+    int val;
+    TreeLinkNode left = null;
+    TreeLinkNode right = null;
+    TreeLinkNode next = null;
+
+    TreeLinkNode(int val) {
+        this.val = val;
+    }
+}
